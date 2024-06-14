3f9b32ddab91b01fdad3e5cf048d4bdfb7ba95e9
yanuarioadimada <yanuarioadimada@gmail.com>
2024-06-14 23:12:45 +0700

    changes

--- register.php
+++ register.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["authenticated"]))
 <!doctype html>
 <html lang="en">
 <head> 
-    <meta charset="utf-8">
+    <meta charset="utf-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
